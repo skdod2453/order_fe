b293385dcace01e20de8bcfde6ca6a46fa51d809
skdod2453 <skdod2453@gmail.com>
2024-11-24 22:55:08 +0900

    fix: Customer 주문 내역 api 연결 및 type에 따른 헤더 변경 완료

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import logo from '../../image/logo.png';
 
 export default function NavBar() {
-  const [cookies, , removeCookie] = useCookies(['Authorization', 'Id']); // 쿠키 가져오기
+  const [cookies, , removeCookie] = useCookies(['Authorization', 'Id', 'Type']);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -25,7 +24,6 @@ export default function NavBar() {
       color: '#754F23'
     }).then((result) => {
       if (result.isConfirmed) {
-        // 사용자가 '예'를 선택한 경우
         removeCookie('Authorization', { path: '/' });
         removeCookie('Id', { path: '/' });
         Swal.fire({
@@ -36,13 +34,14 @@ export default function NavBar() {
           background: '#fff',
           color: '#754F23'
         }).then(() => {
-          navigate('/introduce'); // 로그아웃 후 홈으로 이동
+          navigate('/introduce');
         });
       }
     });
   };
 
   const isLoggedIn = cookies.Authorization && cookies.Id; // 로그인 여부 확인
+  const userType = cookies.Type;
 
   return (
     
@@ -50,8 +49,8 @@ export default function NavBar() {
       <Container fluid>
         <Navbar.Brand>
           <img src={logo} width={50} alt='logo' 
-          style={{ cursor: 'pointer' }} // 클릭 가능하도록 스타일 추가
-          onClick={() => navigate('/introduce')} // 클릭 시 navigate 실행
+          style={{ cursor: 'pointer' }} 
+          onClick={() => navigate('/introduce')} 
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -59,14 +58,25 @@ export default function NavBar() {
           <Nav className="me-auto">
             <Nav.Link href="/home">Store</Nav.Link>
             <Nav.Link href="/map">Map</Nav.Link>
-            <Nav.Link href="/userorderlist">Order list</Nav.Link>
+            {isLoggedIn && userType === 'Customer' && (
+              <Nav.Link href="/userorderlist">Order list</Nav.Link>
+            )}
+            {isLoggedIn && userType === 'Owner' && (
+              <Nav.Link href="/ownermystore">MyStore</Nav.Link>
+            )}
           </Nav>
           <Nav>
             {isLoggedIn ? (
               <>
-                <Nav.Link onClick={handleLogout}>LOGOUT</Nav.Link>
-                <Nav.Link href="/mypage">MYPAGE</Nav.Link>
-              </>
+                {userType === 'Owner' ? (
+                  <Nav.Link onClick={handleLogout}>LOGOUT</Nav.Link>
+                ) : (
+                  <>
+                  <Nav.Link href="/mypage">MYPAGE</Nav.Link>
+                  <Nav.Link onClick={handleLogout}>LOGOUT</Nav.Link>
+                </>
+              )}
+            </>
             ) : (
               <Nav.Link href="/login">LOGIN/JOIN</Nav.Link>
             )}
--- src/pages/User_OrderList.jsx
+++ src/pages/User_OrderList.jsx
@@ -1,91 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/User_OrderList.css';
+import { getOrderCustomer }  from '../apis/restaurant';
 import { useNavigate } from 'react-router-dom';
 import { IoHeartCircleOutline } from "react-icons/io5";
 import { RiMenuSearchLine } from "react-icons/ri";
 import { FcMoneyTransfer } from "react-icons/fc";
 import { HiMiniCalendarDays } from "react-icons/hi2";
+import { useCookies } from 'react-cookie';
+import { toZonedTime, format } from 'date-fns-tz';
 
 export default function User_OrderList() {
   const navigate = useNavigate();
-
-  const orders = [
-    {
-      storeName: '가게 이름1',
-      menuList: ['메뉴1', '메뉴2', '메뉴1'],
-      totalPrice: '30,000원',
-      orderDate: '2024-11-22',
-      chatLink: '#',
-    },
-    {
-      storeName: '가게 이름2',
-      menuList: ['메뉴A', '메뉴B', '메뉴B'],
-      totalPrice: '15,000원',
-      orderDate: '2024-11-21',
-      chatLink: '#',
-    },
-    {
-      storeName: '가게 이름3',
-      menuList: ['메뉴X', '메뉴Y', '메뉴Z', '메뉴X'],
-      totalPrice: '45,000원',
-      orderDate: '2024-11-20',
-      chatLink: '#',
-    },
-    {
-      storeName: '가게 이름3',
-      menuList: ['메뉴X', '메뉴Y', '메뉴Z', '메뉴X'],
-      totalPrice: '45,000원',
-      orderDate: '2024-11-20',
-      chatLink: '#',
-    },
-    {
-      storeName: '가게 이름3',
-      menuList: ['메뉴X', '메뉴Y', '메뉴Z', '메뉴X'],
-      totalPrice: '45,000원',
-      orderDate: '2024-11-20',
-      chatLink: '#',
-    },
-    {
-      storeName: '가게 이름3',
-      menuList: ['메뉴X', '메뉴Y', '메뉴Z', '메뉴X'],
-      totalPrice: '45,000원',
-      orderDate: '2024-11-20',
-      chatLink: '#',
-    },
-    {
-      storeName: '가게 이름3',
-      menuList: ['메뉴X', '메뉴Y', '메뉴Z', '메뉴X', '메뉴A', '메뉴B', '메뉴c'],
-      totalPrice: '45,000원',
-      orderDate: '2024-11-20',
-      chatLink: '#',
-    }
-  ];
+  const [orders, setOrders] = useState([]);
+  const [cookies] = useCookies(['Authorization']);
+  const token = cookies.Authorization;
 
   // 메뉴를 그룹화하는 함수
   const groupMenus = (menuList) => {
-    const menuCount = {};
-    menuList.forEach(menu => {
-      menuCount[menu] = (menuCount[menu] || 0) + 1;
-    });
-    return Object.entries(menuCount).map(([name, count]) => 
-      count > 1 ? `${name} x ${count}` : name
+    return menuList.map((menu) => 
+      menu.count > 1 ? `${menu.menu} x ${menu.count}` : menu.menu
     );
   };
 
+  useEffect(() => {
+    const getCustomerOrderList = async () => {
+      try {
+        const response = await getOrderCustomer(token);
+        setOrders(response.data);
+        console.log(response.data);
+      } catch(error) {
+        console.log(error);
+      }
+    }
+    getCustomerOrderList();
+  }, [token]);
+
+  const formatData = (dataString) => {
+    const data = new Date(dataString.replace('KST', 'UTC'));
+    if (isNaN(data)) {
+      return '날짜 형식 오류';
+    }
+    data.setHours(data.getHours() - 9); // 한국 표준시(KST)로 변환
+  
+    const dateFormatted = data.toLocaleDateString('ko-KR', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    });
+  
+    const timeFormatted = data.toLocaleTimeString('ko-KR', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+  
+    return `${dateFormatted} ${timeFormatted}`;
+  };
+  
   return (
     <div className='user-orderlist-container'>
       <div className='user-orderlist-header'>주문 내역 <RiMenuSearchLine style={{ fontSize: '35px' }}/></div>
       <div className='user-orderlist-content'>
         {orders.map((order, index) => (
           <div className='user-orderlist-item' key={index}>
-            <div style={{ fontWeight: 'bold', fontSize: '18px', color: '#FF7D29' }}>{order.storeName}</div>
+            <div style={{ fontWeight: 'bold', fontSize: '18px', color: '#FF7D29' }}>{order.restaurant}</div>
             <div>
               {groupMenus(order.menuList).map((menu, idx) => (
                 <div key={idx}><IoHeartCircleOutline /> {menu}</div>
               ))}
             </div>
-            <div style={{ fontWeight: 'bold' }}><FcMoneyTransfer /> 총 가격 : {order.totalPrice}</div>
-            <div><HiMiniCalendarDays /> 주문 날짜 : {order.orderDate}</div>
+            <div style={{ fontWeight: 'bold' }}>
+              <FcMoneyTransfer /> 총 가격 : {parseInt(order.amount, 10).toLocaleString()}원
+            </div>
+            <div>
+              <HiMiniCalendarDays /> 주문 날짜 : {formatData(order.timeStamp)}
+            </div>
             <button
               className='order-chat-btn'
               onClick={() => navigate('/chat')}
